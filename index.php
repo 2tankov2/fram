f0991b16e05c29de6dd39155052c52e2223103d1
tankov <tan.kov@hotmail.com>
2020-08-23 18:16:24 +0300

    server fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function server($url)
 {
     if ('/' === $url) {
         return "<p>Welcome to PHP</p>";
-    } else if ('/about' === $url) {
+    } else if (preg_match('/^\/about\/?$/i', $url)) {
         return "about compaty";
     } else if ('/server' === $url) {
         print_r($_SERVER);
